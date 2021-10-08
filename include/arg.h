a995d43fc21544300289bd6bc39cc631eb68c5b8
Benjamin BRASSART <bbrassar@student.42.fr>
2021-10-08 20:05:11 +0200

    Added argument checking header

---
+++ include/arg.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   arg.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/08 17:37:19 by bbrassar          #+#    #+#             */
+/*   Updated: 2021/10/08 17:51:41 by bbrassar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARG_H
+# define ARG_H
+
+# include "ft_def.h"
+
+t_bool	check_arg_int(char const *arg);
+
+t_bool	check_arg_overflow(char const *arg);
+
+void	check_args(int argc, char *argv[]);
+
+t_bool	check_duplicate(int *values, t_size len);
+
+int		*parse_args(int argc, char *argv[], t_size *len_ptr);
+
+#endif
