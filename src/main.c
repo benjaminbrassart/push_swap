b2e4b36c3d3cb768101e39b3cc38737f9eb52519
Benjamin BRASSART <bbrassar@student.42.fr>
2021-10-08 20:09:55 +0200

    Added main

--- src/main.c
+++ src/main.c
@@ -6,13 +6,38 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 16:05:46 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/10/08 16:06:10 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/10/08 19:30:14 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "arg.h"
+#include "ft_stdio.h"
+#include "sort.h"
+#include "stack.h"
+#include <stdlib.h>
+
 int	main(int argc, char *argv[])
 {
-	(void)argc;
-	(void)argv;
+	int		*values;
+	t_size	len;
+
+	check_args(argc, argv);
+	values = parse_args(argc, argv, &len);
+	if (values)
+	{
+		if (!check_duplicate(values, len))
+		{
+			ft_putendl_fd("Error: there are duplicate arguments.", 2);
+			exit(1);
+		}
+		stack_fill(values, len);
+		sort(&_stacks()->a);
+		stack_delete(&_stacks()->a);
+	}
+	else
+	{
+		ft_putendl_fd("Error: memory allocation failed.", 2);
+		exit(1);
+	}
 	return (0);
 }
