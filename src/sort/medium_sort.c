a2bc5c5331bbfe9e564a19dcd629fb052862b609
Benjamin BRASSART <bbrassar@student.42.fr>
2021-10-08 20:13:02 +0200

    Added medium and small sort files

---
+++ src/sort/medium_sort.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   medium_sort.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/08 20:10:25 by bbrassar          #+#    #+#             */
+/*   Updated: 2021/10/08 20:12:25 by bbrassar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "sort.h"
+
+/*
+find median
+push element of a < median in b
+small_sort a, asc
+small_sort b, desc
+push element of b in a
+*/
+void	medium_sort(t_stack *stack)
+{
+	(void)stack;
+}
