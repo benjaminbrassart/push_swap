cde1d8406e5d83ef4f9e2e67c4b8ef9d1cba405d
Benjamin BRASSART <bbrassar@student.42.fr>
2021-10-09 17:33:38 +0200

    Removed g_ops from sort.h

--- include/sort.h
+++ include/sort.h
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 19:29:35 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/10/08 19:55:11 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/10/09 16:44:57 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,6 @@
 # ifdef OPS_COUNT
 #  undef OPS_COUNT
 # endif
-
-# define OPS_COUNT 11
-
-static const char	*g_ops[] = {"pa", "pb", "sa", "sb", "ss", "ra", "rb", "rr",
-	"rra", "rrb", "rrr"};
 
 void	sort(t_stack *stack);
 
