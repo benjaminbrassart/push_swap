32df54fc8f2cdfd2e57440e019d6d2f62ec5d5b9
Benjamin BRASSART <bbrassar@student.42.fr>
2021-10-09 17:34:50 +0200

    Added invalid operation to pserror.h

--- include/pserror.h
+++ include/pserror.h
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 15:02:48 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/10/09 15:55:08 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/10/09 16:59:52 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ typedef enum e_errno
 	MALLOC_FAILED,
 	DUPLICATE_VALUES,
 	NOT_INTEGER,
-	INT_OVERFLOW,	
+	INT_OVERFLOW,
+	INVALID_OPERATION,
 }	t_errno;
 
 struct s_errno_lt
@@ -35,6 +36,7 @@ static const struct s_errno_lt	g_errno[] = {
 	{DUPLICATE_VALUES, "there are duplicate arguments."},
 	{NOT_INTEGER, "an argument is not an integer."},
 	{INT_OVERFLOW, "an argument overflows int limit."},
+	{INVALID_OPERATION, "invalid operation."},
 	{NONE, FT_NULL}
 };
 
