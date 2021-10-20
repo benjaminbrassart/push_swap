3d526ea2090a426dfa6c4e1110f37eb821209477
Benjamin BRASSART <bbrassar@student.42.fr>
2021-10-20 08:33:27 +0200

    Changed g_ops to a function

--- include/op.h
+++ include/op.h
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 16:40:33 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/10/09 18:21:24 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/10/20 08:31:05 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 # define OP_H
 
 # include "ft_def.h"
+
+# define OPERATIONS_COUNT 11
 
 enum e_op
 {
@@ -42,7 +44,7 @@ typedef struct s_op
 union u_op
 {
 	enum e_op	id;
-	char 		*name;
+	char		*name;
 };
 
 t_op				get_op(union u_op op, t_bool by_id);
@@ -75,21 +77,6 @@ void				do_op(union u_op op, t_bool by_id);
 
 void				undo_op(union u_op op, t_bool by_id);
 
-# define OPERATIONS_COUNT 11
-
-static const t_op	g_ops[OPERATIONS_COUNT + 1] = {
-	{PA, "pa", do_pa, do_pb},
-	{PB, "pb", do_pb, do_pa},
-	{SA, "sa", do_sa, do_sa},
-	{SB, "sb", do_sb, do_sb},
-	{SS, "ss", do_ss, do_ss},
-	{RA, "ra", do_ra, do_rra},
-	{RB, "rb", do_rb, do_rrb},
-	{RR, "rr", do_rr, do_rrr},
-	{RRA, "rra", do_rra, do_ra},
-	{RRB, "rrb", do_rrb, do_rb},
-	{RRR, "rrr", do_rrr, do_rr},
-	{INVALID, FT_NULL, invalid_operation, invalid_operation},
-};
+t_op const			*g_ops(void);
 
 #endif
