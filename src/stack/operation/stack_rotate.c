1b325ed72c88445a13056a438937771264dddf56
Benjamin BRASSART <bbrassar@student.42.fr>
2021-10-20 08:33:01 +0200

    Simplified stack_rotate and stack_reverse_rotate

--- src/stack/operation/stack_rotate.c
+++ src/stack/operation/stack_rotate.c
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 17:28:51 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/10/09 17:30:46 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/10/14 07:02:07 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,5 @@
 
 void	stack_rotate(t_stack *stack)
 {
-	t_stack_elem *const	top = stack->first;
-	t_stack_elem *const	bottom = stack->last;
-
-	if (top && bottom && top != bottom)
-	{
-		stack->first = top->next;
-		stack->last = top;
-		top->next->prev = FT_NULL;
-		top->next = FT_NULL;
-		top->prev = bottom;
-		bottom->next = top;
-	}
+	stack_backpush(stack, stack_shift(stack));
 }
