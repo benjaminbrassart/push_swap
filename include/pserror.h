f4896ae9e293a1726a67879ba9043ad6f7a84be9
Benjamin BRASSART <bbrassar@student.42.fr>
2021-10-25 13:36:46 +0200

    Removed g_errno constant

--- include/pserror.h
+++ include/pserror.h
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 15:02:48 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/10/20 08:31:46 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/10/20 08:54:36 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,15 +31,9 @@ struct s_errno_lt
 	char const	*message;
 };
 
-static struct s_errno_lt const	g_errno[] = {
-	{MALLOC_FAILED, "Memory allocation failed"},
-	{DUPLICATE_VALUES, "There are duplicate arguments"},
-	{NOT_INTEGER, "An argument is not an integer"},
-	{INT_OVERFLOW, "An argument overflows int limit"},
-	{INVALID_OPERATION, "Invalid operation"},
-	{NONE, FT_NULL}
-};
+struct s_errno_lt const	*g_errno(void);
 
-void	psexit(t_errno errno) __attribute__((noreturn));
+void					psexit(t_errno errno)
+						__attribute__((noreturn));
 
 #endif
