3e7d80b14fab5f59870202bced4cfd7b099527cf
Benjamin BRASSART <bbrassar@student.42.fr>
2021-11-03 05:19:22 +0100

    Used psexit in check_arg_overflow

--- src/arg/check_arg_overflow.c
+++ src/arg/check_arg_overflow.c
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 17:39:48 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/10/08 18:38:58 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/11/02 00:10:38 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include "ft_stdlib.h"
 #include "ft_stdio.h"
 #include "ft_string.h"
+#include "pserror.h"
 #include <stdlib.h>
 
 t_bool	check_arg_overflow(char const *arg)
@@ -28,6 +29,5 @@ t_bool	check_arg_overflow(char const *arg)
 		free(s);
 		return (inbound);
 	}
-	ft_putendl_fd("Error: memory allocation failed.", 2);
-	exit(1);
+	psexit(MALLOC_FAILED);
 }
