298e6bbd5e91d833c3cc3c598647cbee4cd36506
Benjamin BRASSART <bbrassar@student.42.fr>
2021-11-03 15:38:53 +0100

    Added stack_remap

--- include/util.h
+++ include/util.h
@@ -6,13 +6,14 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 17:26:40 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/11/02 00:03:43 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/11/03 14:44:30 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef UTIL_H
 # define UTIL_H
 
+# include "ft_def.h"
 # include "stack_type.h"
 
 typedef struct s_stacks
@@ -26,6 +27,12 @@ t_stacks	*_stacks(void);
 t_stack		*opposite_stack(t_stack *stack)
 			__attribute__((nonnull(1)));
 
-void		swap_i(int *a, int *b);
+void		stack_print(t_stack const *stack);
+
+t_bool		stack_remap(t_stack *stack)
+			__attribute__((nonnull(1)));
+
+void		swap_i(int *a, int *b)
+			__attribute__((nonnull(1, 2)));
 
 #endif
