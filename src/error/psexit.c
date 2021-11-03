1740fcf87b230be17c5b157de8bfeb06e65b732a
Benjamin BRASSART <bbrassar@student.42.fr>
2021-11-03 05:31:43 +0100

    Added debug statements in psexit

--- src/error/psexit.c
+++ src/error/psexit.c
@@ -6,13 +6,14 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 15:09:07 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/10/25 13:38:40 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/11/02 00:01:55 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_stdio.h"
 #include "pserror.h"
 #include "stack.h"
+#include "util.h"
 #include <stdlib.h>
 
 static char const	*_get_errno_message(t_errno err)
@@ -26,10 +27,21 @@ static char const	*_get_errno_message(t_errno err)
 	return (_errno[i].message);
 }
 
+static int	_print_elem(t_stack_elem *e)
+{
+	ft_printf("%p = %d (prev: %p, next: %p)\n", e, e->value, e->prev,
+		e->next);
+	return (1);
+}
+
 void	psexit(t_errno err)
 {
-	stack_delete(&_stacks()->a);
-	stack_delete(&_stacks()->b);
+	ft_printf("A\n");
+	stack_foreach(&_stacks()->a, _print_elem);
+	ft_printf("B\n");
+	stack_foreach(&_stacks()->b, _print_elem);
+	stack_delete(&_stacks()->a, FT_NULL);
+	stack_delete(&_stacks()->b, FT_NULL);
 	if (err != NONE)
 	{
 		ft_putstr_fd("Error: ", 2);
