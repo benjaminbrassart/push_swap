576aa0794d912a71a843c5b7daa4ff9ec5ba2dc0
Benjamin BRASSART <bbrassar@student.42.fr>
2021-11-03 05:21:26 +0100

    Included util.h in opposite_stack

--- src/stack/_stacks.c
+++ src/stack/_stacks.c
@@ -6,16 +6,16 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 16:21:34 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/10/08 19:01:34 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/10/28 22:57:31 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_def.h"
-#include "stack.h"
+#include "util.h"
 
-struct s_stacks	*_stacks(void)
+t_stacks	*_stacks(void)
 {
-	static struct s_stacks	stacks = {
+	static t_stacks	stacks = {
 		{'a', FT_NULL, FT_NULL, 0},
 		{'b', FT_NULL, FT_NULL, 0}
 	};
--- src/stack/opposite_stack.c
+++ src/stack/opposite_stack.c
@@ -6,11 +6,12 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 17:45:28 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/10/28 17:48:08 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/11/02 00:04:01 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "stack.h"
+#include "util.h"
 
 t_stack	*opposite_stack(t_stack *stack)
 {
