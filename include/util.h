69db7de404deb115931e9ffb0eae911d918d169b
Benjamin BRASSART <bbrassar@student.42.fr>
2021-11-03 05:19:56 +0100

    Added util.h to provide _stacks() and opposite_stack()

--- include/util.h
+++ include/util.h
@@ -6,13 +6,26 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 17:26:40 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/10/09 17:26:56 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/11/02 00:03:43 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef UTIL_H
 # define UTIL_H
 
-void	swap_i(int *a, int *b);
+# include "stack_type.h"
+
+typedef struct s_stacks
+{
+	t_stack	a;
+	t_stack	b;
+}	t_stacks;
+
+t_stacks	*_stacks(void);
+
+t_stack		*opposite_stack(t_stack *stack)
+			__attribute__((nonnull(1)));
+
+void		swap_i(int *a, int *b);
 
 #endif
