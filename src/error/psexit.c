4dbb3fa43f76afacb83ebad513ffd97ac879fa16
Benjamin BRASSART <bbrassar@student.42.fr>
2021-11-06 16:27:09 +0100

    Removed comments in psexit

--- src/error/psexit.c
+++ src/error/psexit.c
@@ -6,11 +6,12 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 15:09:07 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/11/03 10:42:01 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/11/06 16:27:01 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_stdio.h"
+#include "oplist.h"
 #include "pserror.h"
 #include "stack.h"
 #include "util.h"
@@ -27,19 +28,8 @@ static char const	*_get_errno_message(t_errno err)
 	return (_errno[i].message);
 }
 
-// static int	_print_elem(t_stack_elem *e)
-// {
-// 	ft_printf("%p = %d (prev: %p, next: %p)\n", e, e->value, e->prev,
-// 		e->next);
-// 	return (1);
-// }
-
 void	psexit(t_errno err)
 {
-	// ft_printf("A\n");
-	// stack_foreach(&_stacks()->a, _print_elem);
-	// ft_printf("B\n");
-	// stack_foreach(&_stacks()->b, _print_elem);
 	stack_delete(&_stacks()->a, FT_NULL);
 	stack_delete(&_stacks()->b, FT_NULL);
 	if (err != NONE)
