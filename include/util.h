1dbcc40d624d5e70a82e4aea49fdd47eeeabc36e
Benjamin BRASSART <bbrassar@student.42.fr>
2021-11-06 07:53:03 +0100

    Added some documentation to util.h

--- include/util.h
+++ include/util.h
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 17:26:40 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/11/03 14:44:30 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/11/04 10:11:41 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,53 @@ typedef struct s_stacks
 	t_stack	b;
 }	t_stacks;
 
-t_stacks	*_stacks(void);
+t_stacks		*_stacks(void);
 
-t_stack		*opposite_stack(t_stack *stack)
-			__attribute__((nonnull(1)));
+t_stack			*opposite_stack(t_stack *stack)
+				__attribute__((nonnull(1)));
 
-void		stack_print(t_stack const *stack);
+/**
+ * Print the element of a stack
+ * 
+ * @param stack the stack to print
+ */
+void			stack_print(t_stack const *stack)
+				__attribute__((nonnull(1)));
 
-t_bool		stack_remap(t_stack *stack)
-			__attribute__((nonnull(1)));
+/**
+ * Map a stack and replace each number by its order
+ * 
+ * @param stack the stack to map
+ * @return true if the mapping went flawlessly, false otherwise
+ */
+t_bool			stack_remap(t_stack *stack)
+				__attribute__((nonnull(1)));
 
-void		swap_i(int *a, int *b)
-			__attribute__((nonnull(1, 2)));
+/**
+ * Swap two integers
+ * 
+ * @param a a pointer to the first integer
+ * @param b a pointer to the second integer
+ */
+void			swap_i(int *a, int *b)
+				__attribute__((nonnull(1, 2)));
+
+/**
+ * Get the most significant bit of a number
+ * 
+ * @param n the number
+ * @return the position of the most significant bit that equals 1
+ */
+unsigned int	get_most_significant_bit(int n);
+
+/**
+ * Get the highest most significant bit of a sequence of numbers
+ * 
+ * @param values the sequence of numbers
+ * @param n the number of values
+ * @return the highest most significant bit that equals 1
+ */
+unsigned int	get_highest_most_significant_bit(int *values, unsigned int n)
+				__attribute__((nonnull(1)));
 
 #endif
