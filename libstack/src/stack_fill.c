7a24a9e6557fc172dd1a6c4a52dfc90e3ffb184b
Benjamin BRASSART <bbrassar@student.42.fr>
2021-11-07 18:52:19 +0100

    Removed include of stdlib in stack_fill

--- libstack/src/stack_fill.c
+++ libstack/src/stack_fill.c
@@ -6,12 +6,11 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 23:00:58 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/10/28 23:06:41 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/11/07 18:50:48 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "stack_function.h"
-#include <stdlib.h>
 
 int	stack_fill(t_stack *stack, int *values, unsigned int length)
 {
@@ -22,9 +21,9 @@ int	stack_fill(t_stack *stack, int *values, unsigned int length)
 	while (++i < length)
 	{
 		elem = stack_elem_new(values[i]);
-		if (elem == NULL)
+		if (!elem)
 		{
-			stack_delete(stack, NULL);
+			stack_delete(stack, 0);
 			return (0);
 		}
 		stack_backpush(stack, elem);
