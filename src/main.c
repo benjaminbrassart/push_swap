69afc93e4d83497fa84fd47a4709e24b2e7bae0a
Benjamin BRASSART <bbrassar@student.42.fr>
2021-11-07 18:53:44 +0100

    Updated main file

--- src/main.c
+++ src/main.c
@@ -6,35 +6,67 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 16:05:46 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/11/06 08:14:18 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/11/07 18:51:16 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "arg.h"
+#include "ft_stdio.h"
 #include "pserror.h"
 #include "sort.h"
 #include "stack.h"
 #include "util.h"
+#include <stdlib.h>
+
+static int	_get_position(int i, int *values, unsigned int len)
+{
+	unsigned int	n;
+
+	n = 0;
+	while (n < len)
+	{
+		if (values[n] == i)
+			return ((int)n);
+		++n;
+	}
+	return (-1);
+}
+
+static void	_exit_free_values(int *values, t_errno error)
+{
+	free(values);
+	psexit(error);
+}
+
+static void	_values_remap(int *values, unsigned int len)
+{
+	unsigned int	n;
+	int *const		new_values = insertion_sort(values, len);
+
+	if (!new_values)
+		_exit_free_values(values, MALLOC_FAILED);
+	n = -1;
+	while (++n < len)
+		values[n] = _get_position(values[n], new_values, len);
+	free(new_values);
+	if (!stack_fill(&_stacks()->a, values, len))
+		_exit_free_values(values, MALLOC_FAILED);
+}
 
 int	main(int argc, char *argv[])
 {
-	t_stack *const	a = &_stacks()->a;
 	int				*values;
 	t_size			len;
-	unsigned int	msb;
 
 	check_args(argc, argv);
 	values = parse_args(argc, argv, &len);
-	if (!values)
-		psexit(MALLOC_FAILED);
 	if (!check_duplicate(values, len))
-		psexit(DUPLICATE_VALUES);
-	msb = get_highest_most_significant_bit(values, len);
-	stack_fill(a, values, len);
-	stack_remap(a);
-	if (a->size < 7)
+		_exit_free_values(values, DUPLICATE_VALUES);
+	_values_remap(values, len);
+	free(values);
+	if (_stacks()->a.size < 7)
 		sort_small();
 	else
-		sort(msb);
+		sort(get_highest_most_significant_bit(values, len));
 	psexit(NONE);
 }
