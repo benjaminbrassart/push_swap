fe445cc750ca12c5abdc06088472c5772edbb5c7
Benjamin BRASSART <bbrassar@student.42.fr>
2021-11-07 18:52:49 +0100

    Added error message for int underflow

--- src/error/g_errno.c
+++ src/error/g_errno.c
@@ -6,10 +6,11 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/20 08:54:03 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/10/20 08:55:00 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/11/07 17:42:10 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_def.h"
 #include "pserror.h"
 
 struct s_errno_lt const	*g_errno(void)
@@ -19,6 +20,7 @@ struct s_errno_lt const	*g_errno(void)
 		{DUPLICATE_VALUES, "There are duplicate arguments"},
 		{NOT_INTEGER, "An argument is not an integer"},
 		{INT_OVERFLOW, "An argument overflows int limit"},
+		{INT_UNDERFLOW, "An argument underflows int limit"},
 		{INVALID_OPERATION, "Invalid operation"},
 		{NONE, FT_NULL}
 	};
